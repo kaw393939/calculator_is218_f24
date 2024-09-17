1972e35b591c0594ad7d7541ddcd0ed42bd7688c
kaw393939 <kwilliam@njit.edu>
2024-09-17 16:48:00 -0400

    added subtraction

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 # addition function 
 def addition(a,b):
     return a + b
+
+# subtraction function
+def subtraction(a,b):
+    return a - b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 ''' My Calculator Test'''
-from app.main import addition
+from app.main import addition, subtraction
 
 def test_addition():
     '''Addition funtion'''
     assert addition(1,1)  == 2
+
+def test_subtraction():
+    '''Addition funtion'''
+    assert subtraction(1,1)  == 0
