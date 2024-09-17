61ddf092ec03be8dd0998848e323a819f2bcd265
kaw393939 <kwilliam@njit.edu>
2024-09-17 17:18:53 -0400

    added calculator functions and tests

--- app/main.py
+++ app/main.py
@@ -5,3 +5,9 @@ def addition(a,b):
 # subtraction function
 def subtraction(a,b):
     return a - b
+
+def multiplication(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,25 @@
 ''' My Calculator Test'''
-from app.main import addition, subtraction
+import pytest
+from app.main import addition, division, multiplication, subtraction
 
 def test_addition():
     '''Addition funtion'''
     assert addition(1,1)  == 2
 
 def test_subtraction():
-    '''Addition funtion'''
+    '''Subtraction funtion'''
     assert subtraction(1,1)  == 0
+
+def test_multiplication():
+    '''Subtraction funtion'''
+    assert multiplication(2,2)  == 4
+
+def test_division():
+    '''Division Function'''
+    assert division(2,2) == 1
+
+
+def test_division_by_zero_exception():
+    '''Division Function testing that I get the exception divide by zero'''
+    with pytest.raises(ZeroDivisionError):
+        division(10,0)
