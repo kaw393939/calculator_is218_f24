9840f933b42300a2f4ac5f4ac710a4a022780b1b
kaw393939 <kwilliam@njit.edu>
2024-09-24 16:40:25 -0400

    added calculator

---
+++ app/calculator/__init__.py
@@ -0,0 +1,15 @@
+# Import your operations module functions
+from app.operations import addition, subtraction, multiplication, division
+
+class Calculator:
+    def add(self, a, b):
+        return addition(a, b)
+
+    def subtract(self, a, b):
+        return subtraction(a, b)
+
+    def multiply(self, a, b):
+        return multiplication(a, b)
+
+    def divide(self, a, b):
+        return division(a, b)
